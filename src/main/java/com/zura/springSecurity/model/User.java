c08c11c1f6ba3bb82dfd7069f919ad71941ab946
Zura <jur.and.jr42@gmail.com>
2023-10-23 17:33:52 -0300

    Created packages and User model class

---
+++ src/main/java/com/zura/springSecurity/model/User.java
@@ -0,0 +1,26 @@
+package com.zura.springSecurity.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "userdata")
+public class User {
+    @Id
+    @Column
+    @SequenceGenerator(
+            name = "id_generator",
+            sequenceName = "id_generator",
+            allocationSize = 1
+    )
+    @GeneratedValue (
+            strategy = GenerationType.AUTO,
+            generator = "id_genrator"
+    )
+    private Long id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String email;
+}
