7457ad1d097084070f98b8b2cc16cf7982e3acbc
Zura <jur.and.jr42@gmail.com>
2023-10-24 15:20:00 -0300

    Added Spring annotations to User and made it implement UserDetails

--- src/main/java/com/zura/springSecurity/model/User.java
+++ src/main/java/com/zura/springSecurity/model/User.java
@@ -1,10 +1,22 @@
 package com.zura.springSecurity.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "userdata")
-public class User {
+public class User implements UserDetails {
     @Id
     @Column
     @SequenceGenerator(
@@ -25,4 +37,34 @@ public class User {
     private String email;
     @Column
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
