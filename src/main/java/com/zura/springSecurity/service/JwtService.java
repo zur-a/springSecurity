74c9a981bbddc101155aaae1ab2c85ed8b26bb7c
Zura <jur.and.jr42@gmail.com>
2023-10-26 17:36:54 -0300

    Added logic to generate a jwt token to JwtService'

--- src/main/java/com/zura/springSecurity/service/JwtService.java
+++ src/main/java/com/zura/springSecurity/service/JwtService.java
@@ -2,11 +2,17 @@ package com.zura.springSecurity.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -16,6 +22,21 @@ public class JwtService {
 
     public String extractUsername(String jwtToken) {
         return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
     }
 
     private Claims extractAllClaims(String jwtToken) {
@@ -31,6 +52,8 @@ public class JwtService {
         final Claims claims = extractAllClaims(jwtToken);
         return claimsResolver.apply(claims);
     }
+
+
 
     private Key getSignInKey() {
         byte[] keyByte = Decoders.BASE64.decode(SIGN_IN_KEY);
