1b9ef00c633b330eb6b051caa3e5dd2c5688bc6f
Zura <jur.and.jr42@gmail.com>
2023-10-26 16:30:57 -0300

    Implemented logic to extract username in JwtService

--- src/main/java/com/zura/springSecurity/service/JwtService.java
+++ src/main/java/com/zura/springSecurity/service/JwtService.java
@@ -1,10 +1,39 @@
 package com.zura.springSecurity.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    //TODO: Move sign in key to application properties
+    private static final String SIGN_IN_KEY = "d905b8386577260a22eccd4027b609922e084b9fab7127972f99d7f66fca9c05";
+
     public String extractUsername(String jwtToken) {
-        return "";
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwtToken);
+        return claimsResolver.apply(claims);
+    }
+
+    private Key getSignInKey() {
+        byte[] keyByte = Decoders.BASE64.decode(SIGN_IN_KEY);
+        return Keys.hmacShaKeyFor(keyByte);
     }
 }
