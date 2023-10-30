825350aafdd068ad99f56a02412d7522c727f932
Zura <jur.and.jr42@gmail.com>
2023-10-30 16:38:43 -0300

    Implementation of AuthenticationRequest

--- src/main/java/com/zura/springSecurity/request/AuthenticationRequest.java
+++ src/main/java/com/zura/springSecurity/request/AuthenticationRequest.java
@@ -1,4 +1,15 @@
 package com.zura.springSecurity.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationRequest {
+    private String email;
+    private String password;
 }
