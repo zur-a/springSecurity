35e9bb2f9873674074ee5092d7957bdf4eccdf6b
Zura <jur.and.jr42@gmail.com>
2023-10-30 14:16:30 -0300

    Implemented beans for fetching user details and pass encoding in ApplicationConfig

--- src/main/java/com/zura/springSecurity/utils/ApplicationConfig.java
+++ src/main/java/com/zura/springSecurity/utils/ApplicationConfig.java
@@ -4,18 +4,37 @@ import com.zura.springSecurity.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
     private final UserRepository repository;
+    private final BCryptPasswordEncoder encoder;
     @Bean
     public UserDetailsService userDetailsService() {
         return emailUsername -> repository
                                     .findByEmail(emailUsername)
                                     .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+    }
+
+    //Data Access Object responsible for fetching the user details and encoding the password
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(encoder);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
