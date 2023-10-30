b7c0ab055602b4b685f572c599d863ce268a2a75
Zura <jur.and.jr42@gmail.com>
2023-10-30 13:11:03 -0300

    Implementation of Application config

--- src/main/java/com/zura/springSecurity/utils/ApplicationConfig.java
+++ src/main/java/com/zura/springSecurity/utils/ApplicationConfig.java
@@ -1,5 +1,21 @@
 package com.zura.springSecurity.utils;
 
-public class ApplicationConfig {
+import com.zura.springSecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository repository;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return emailUsername -> repository
+                                    .findByEmail(emailUsername)
+                                    .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+    }
 }
