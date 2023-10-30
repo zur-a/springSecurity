e7e142ffdee617f776e30ada499b8152f1f00eb6
Zura <jur.and.jr42@gmail.com>
2023-10-30 13:18:15 -0300

    Implementeds token validation logic in JwtAutenticationFilter

--- src/main/java/com/zura/springSecurity/security/JwtAuthenticationFilter.java
+++ src/main/java/com/zura/springSecurity/security/JwtAuthenticationFilter.java
@@ -7,9 +7,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -43,6 +45,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             //User is not authenticated if getAuthentication is null
         if (emailUsername != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(emailUsername);
+
+            //Validating and checking if the token is still valid
+            if (jwtService.isJwtTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                //Updating the SecurityContextHolder
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
         }
+        filterChain.doFilter(request, response);
     }
 }
